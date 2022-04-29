ba0a3240cc5e4329daa6e80553ec3c3acf12eeb4
walterbaya <walterbaya@yahoo.com>
2022-04-28 22:56:48 -0300

    agregamos clase de angular sobre ngIf y routing

--- Angular/AprendiendoAngular/src/app/app.module.ts
+++ Angular/AprendiendoAngular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {routing, appRoutingProviders} from './app.routing';
 import { AppComponent } from './app.component';
 import { MiComponente } from './components/mi-componente/mi-componente.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
@@ -9,6 +9,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +24,18 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     SliderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    FormularioComponent,
+    PaginaComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
